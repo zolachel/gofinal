10213763114d3fe32a44f71144e6c3764f4ba9eb
AnuchitO <anuchit.prasertasng@gmail.com>
2020-08-27 20:45:33 +0700

    initialize

---
+++ README.md
@@ -0,0 +1,3 @@
+# Required Action
+1. Please set up go module
+1. MUST start api at port :2009
---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("customer service")
+	//run port ":2009"
+}
